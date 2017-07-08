1bc8238d0bfca6b5ce2da310c81a9c25e06418e0
Marcelo Dominguez <marcelo@mimiquate.com>
2017-07-08 20:40:42 -0300

    Test

--- nerves.exs
+++ nerves.exs
@@ -11,9 +11,6 @@ config pkg, :nerves_env,
   type: :system,
   version: version,
   compiler: :nerves_package,
-  artifact_url: [
-    "https://github.com/nerves-project/#{pkg}/releases/download/v#{version}/#{pkg}-v#{version}.tar.gz",
-  ],
   platform: Nerves.System.BR,
   platform_config: [
     defconfig: "nerves_defconfig"
